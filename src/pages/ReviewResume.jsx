f259b8059dc9fdb55dd8052a65d1ccda114494b9
kaifuddin009 <kaifuddin01@email.com>
2025-08-23 18:25:18 +0530

    add:implement Review resume section

--- src/pages/ReviewResume.jsx
+++ src/pages/ReviewResume.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { File,FileText,Sparkles } from 'lucide-react';
+import React,{useState} from 'react';
+
 
 const ReviewResume = () => {
+  const [input, setInput] = useState('');
+          const onSubmitHandler = async(e)=>{
+            e.preventDefault();
+          }
   return (
-    <div>
-      
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      {/*let col */}
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#00daf3]'/>
+          <h1 className='text-xl font-semibold'>Resume Review</h1>
+        </div>
+        
+          <p className='mt-6 text-md font-semibold'>Upload Resume</p>
+          <input onChange = {(e) =>setInput(e.target.files[0])} type="file"
+          accept='application/pdf' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required/>
+         
+        
+          <p className='text-xs mt-1 '>Supports PDF resume only</p>
+          <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00daf3] to-[#009bb3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+            <FileText className='w-5'/>
+          Review Resume
+          </button>
+      </form>
+        {/*right col */}
+        <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+          
+          <div className='flex items-center gap-3'>
+            <FileText className='w-6 h-6 text-[#00daf3]'/>
+          <h1 className='text-2xl font-semibold'>Analysis Result</h1>
+          </div>
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+
+          <File className='w-9 h-9 cursor-pointer'/>
+
+          <p className='text-lg'>Upload your resume and click "Review Resume" to get started</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
